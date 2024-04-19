90126aed950a7c4ec4e59dcf48297cbcc5f16108
Jitca Diana <jitcadiana6@gmail.com>
2024-04-19 21:06:39 +0300

    calendar + main page students : done responsive : done

--- src/Main-page-students/Main-Page-Students.tsx
+++ src/Main-page-students/Main-Page-Students.tsx
@@ -8,9 +8,18 @@ function MainPageStudents(){
             <Header/>
             <h1 className="title">Fii student la <em>FII</em>.</h1>
             <div className="main">
-
-                <div className="calendar">
+                <div className="buttons">
+                    <button type="button" className="btn btn-outline-primary" data-mdb-ripple-init
+                            data-mdb-ripple-color="dark">Semester 1
+                    </button>
+                    <button type="button" className="btn btn-outline-primary" data-mdb-ripple-init
+                            data-mdb-ripple-color="dark">Semester 2
+                    </button>
+                </div>
+                <div className="calendar-container">
+                    <div className="calendar">
                     <MyCalendar/>
+                    </div>
                 </div>
             </div>
         </div>
